58b50f49b711acf9ebed88571261c8c597c6c608
FariaOaishi <oaishi.faria@gmail.com>
2021-12-02 05:40:23 +0600

    research-page-updated!

--- js/theme.js
+++ js/theme.js
@@ -66,6 +66,25 @@
 			}
 		});
 	}
+	
+	if ($('.popup-with-zoom-anim').length > 0) {
+		$('.popup-with-zoom-anim').magnificPopup({
+			type: 'inline',
+
+			fixedContentPos: false,
+			fixedBgPos: true,
+
+			overflowY: 'auto',
+
+			closeBtnInside: true,
+			preloader: false,
+			
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-zoom-in'
+		});
+	}
+	
 	/*----------------------------------------------------*/
 	/*  End  Magnific Pop Up
 	/*----------------------------------------------------*/
